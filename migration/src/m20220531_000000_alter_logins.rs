f4948b672404510eed1d8927f43de937ec2c9394
sjud <64875465+sjud@users.noreply.github.com>
2022-05-31 15:30:01 -0400

    login entity generated

--- migration/src/m20220531_000000_alter_logins.rs
+++ migration/src/m20220531_000000_alter_logins.rs
@@ -1,4 +1,5 @@
-use sea_orm::{ConnectionTrait, Statement};
+use sea_orm::{ConnectionTrait, Statement, TryFromU64};
+use sea_orm::prelude::DateTimeWithTimeZone;
 use sea_orm_migration::prelude::*;
 pub struct Migration;
 
@@ -7,19 +8,22 @@ impl MigrationName for Migration {
         file!()
     }
 }
-#[Iden]
-pub enum Logins{
+use sea_orm::sea_query::Iden;
+#[derive(Iden)]
+pub enum Logins {
     Table,
-};
+    UserUuid,
+    Email,
+    Password,
+}
+
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let table = Table::alter()
             .table(Logins::Table)
             .add_column(ColumnDef::new(Alias::new("last_login"))
-                .timestamp()
-                .not_null()
-                .default(0))
+                .timestamp_with_time_zone())
             .add_column(ColumnDef::new(Alias::new("is_validated"))
                 .boolean()
                 .not_null()
